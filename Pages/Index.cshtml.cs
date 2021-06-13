21b96abfbf5a9f582434a2b35de176222e6e0833
Ginanjar <ganjar56@gmail.com>
2021-06-14 01:00:29 +0700

    Adding Bootstrap and layouting, also template

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using Model.Model.UserMgt;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MiniInvoice.Pages
 {
-    [Serializable]
     public class IndexModel : PageModel
     {
         
         public string Username { get; set; }
         public string Password { get; set; }
         
-
         public void OnGet()
         {
+            
+
         }
     }
 }
--- Pages/Main/Invoice.cshtml.cs
+++ Pages/Main/Invoice.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MiniInvoice.Pages
+namespace MiniInvoice.Pages.Main
 {
     public class InvoiceModel : PageModel
     {
