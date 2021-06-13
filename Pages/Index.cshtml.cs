fe57f655de5dc62d97f0560b073382497feb68ff
Ginanjar <ganjar56@gmail.com>
2021-06-13 21:41:55 +0700

    make it better

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Model.Model.UserMgt;
+
+namespace MiniInvoice.Pages
+{
+    [Serializable]
+    public class IndexModel : PageModel
+    {
+        
+        public string Username { get; set; }
+        public string Password { get; set; }
+        
+
+        public void OnGet()
+        {
+        }
+    }
+}
